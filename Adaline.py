2978b821bf02188b1ce01077ff301c13d9261d1d
MtheEPIC <mtheepic1423@protonmail.com>
2021-04-25 20:35:40 +0300

    changed weights

--- Adaline.py
+++ Adaline.py
@@ -13,20 +13,30 @@ class CustomAdaline(object):
         self.n_iterations = n_iterations
         self.random_state = random_state
         self.learning_rate = learning_rate
-        self.coef_ = None
+        self.weights = None
         self.score_ = None
 
     def fit(self, X: np.array, y: np.array) -> None:
         rgen = np.random.RandomState(self.random_state)
-        self.coef_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.weights = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        # self.weights[1:] = self.weights[1:] / (np.sum(self.weights[1:]))
         for _ in range(self.n_iterations):
-            activation_function_output = self.activation_function(self.net_input(X))
-            errors = y - activation_function_output
-            self.coef_[1:] = self.coef_[1:] + self.learning_rate * X.T.dot(errors)
-            self.coef_[0] = self.coef_[0] + self.learning_rate * errors.sum()
+            for row in range(X.shape[0]):
+                # activation_function_output = self.activation_function(self.net_input(X))
+                activation_function_output = self.net_input(X[row])
+                # errors = (y - activation_function_output)**2
+                error = y[row] - activation_function_output
+                # errors = (y - activation_function_output)
+                # errors = np.sqrt(y**2 - activation_function_output**2)
+                # self.weights[1:] = self.weights[1:] + self.learning_rate * X.T.dot(errors)
+                self.weights[1:] = self.weights[1:] + self.learning_rate * error
+                # self.weights[1:] = self.weights[1:] / (np.sum(self.weights[1:]))
+                # self.weights[0] = self.weights[0] + self.learning_rate * errors.sum()
+                self.weights[0] = self.weights[0] + self.learning_rate * error
+
 
     def net_input(self, X: np.array) -> np.array:
-        weighted_sum = np.dot(X, self.coef_[1:]) + self.coef_[0]
+        weighted_sum = np.dot(X, self.weights[1:]) + self.weights[0]
         return weighted_sum
 
     @staticmethod
@@ -34,25 +44,17 @@ class CustomAdaline(object):
         return X
 
     def predict(self, X: np.array) -> np.array:
-        return np.where(self.activation_function(self.net_input(X)) >= 0.0, 1, 0)
-
-    # def score(self, X: np.array, y: np.array) -> np.array:
-    #     misclassified_data_count = 0
-    #     for xi, target in zip(X, y):
-    #         output = self.predict(xi)
-    #         if target != output:
-    #             misclassified_data_count += 1
-    #     total_data_count = len(X)
-    #     self.score_ = (total_data_count - misclassified_data_count) / total_data_count
-    #     return self.score_
+        # return np.where(self.activation_function(self.net_input(X)) >= 0.0, -1, 1)
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
 
     def score(self, y_true: np.array, y_pred: np.array) -> float:
-        self.score_ = np.sum(y_true == y_pred) / len(y_true)
+        self.score_ = np.sum(y_true == y_pred) / len(y_true) * 100
         return self.score_
 
     @staticmethod
     def prepare_data(resolution: int) -> np.array:
         data = np.zeros(shape=(VECTOR_SIZE, 3))
+        np.random.seed(101)
         data[:, :2] = np.random.randint(-resolution, resolution + 1, size=(VECTOR_SIZE, 2)) / resolution
 
         for row in data:
@@ -62,7 +64,6 @@ class CustomAdaline(object):
 
 
 if __name__ == '__main__':
-    print("here")
     adaline = CustomAdaline()
     data = adaline.prepare_data(100)
 
